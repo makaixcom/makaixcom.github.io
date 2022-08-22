9f75750ea14e378819ef0075f8db97cc9ab334b4
makaixcom <makaixcom@users.noreply.github.com>
2022-08-22 02:08:35 +0000

    deploy: makaixcom/blog@d41a87bbc6a4376eb1550d8283273634262834eb

---
+++ assets/002-mirror.html.665b961b.js
@@ -0,0 +1,13 @@
+import{_ as n,o as s,c as e,e as a}from"./app.0e1cb0b9.js";const i={},t=a(`<h2 id="\u547D\u4EE4" tabindex="-1"><a class="header-anchor" href="#\u547D\u4EE4" aria-hidden="true">#</a> \u547D\u4EE4</h2><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code><span class="token function">sudo</span> <span class="token function">mkdir</span> -p /etc/docker
+<span class="token function">sudo</span> <span class="token function">tee</span> /etc/docker/daemon.json <span class="token operator">&lt;&lt;-</span><span class="token string">&#39;EOF&#39;
+{
+  &quot;registry-mirrors&quot;: [
+    &quot;https://registry.docker-cn.com&quot;,
+    &quot;http://hub-mirror.c.163.com&quot;,
+    &quot;https://docker.mirrors.ustc.edu.cn&quot;
+  ]
+}
+EOF</span>
+<span class="token function">sudo</span> systemctl daemon-reload
+<span class="token function">sudo</span> systemctl restart <span class="token function">docker</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),r=[t];function c(o,d){return s(),e("div",null,r)}var u=n(i,[["render",c],["__file","002-mirror.html.vue"]]);export{u as default};
---
+++ assets/settings.html.579d14b0.js
@@ -0,0 +1,41 @@
+import{_ as e,o as i,c as n,e as a}from"./app.0e1cb0b9.js";const d={},s=a(`<h2 id="_1-\u4EE3\u7801\u533A\u5B57\u4F53\u5927\u5C0F\u6837\u5F0F" tabindex="-1"><a class="header-anchor" href="#_1-\u4EE3\u7801\u533A\u5B57\u4F53\u5927\u5C0F\u6837\u5F0F" aria-hidden="true">#</a> 1 \u4EE3\u7801\u533A\u5B57\u4F53\u5927\u5C0F\u6837\u5F0F</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>\u4F4D\u7F6E Settings --&gt; Editor--&gt; Color Scheme --&gt; Color Scheme Font 
+\u4FEE\u6539\u4EE5\u4E0B
+Use color scheme font instead of the default \u6253\u94A9
+Font: Yahei Consolas Hybrid
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_2-\u8BBE\u7F6E\u7F16\u7801\u5B57\u7B26\u96C6" tabindex="-1"><a class="header-anchor" href="#_2-\u8BBE\u7F6E\u7F16\u7801\u5B57\u7B26\u96C6" aria-hidden="true">#</a> 2 \u8BBE\u7F6E\u7F16\u7801\u5B57\u7B26\u96C6</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>\u4F4D\u7F6E\uFF1ASettings --&gt; Editor --&gt; File Encodings
+\u4FEE\u6539\u4EE5\u4E0B\u5185\u5BB9
+Global Encoding --&gt; UTF-8
+Project Encoding --&gt; UTF-8
+Default Encoding for properties files --&gt; UTF-8 
+Transparent native-to-ascii conversion  --&gt; \u6253\u94A9
+create UTF-8 file --&gt; with no BOM
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_3-\u914D\u7F6Ejdk" tabindex="-1"><a class="header-anchor" href="#_3-\u914D\u7F6Ejdk" aria-hidden="true">#</a> 3 \u914D\u7F6EJDK</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>3.1 \u9879\u76EE\u7F16\u8BD1\u7684JDK\u7248\u672C
+build, Execution Depolyment --&gt; Compiler --&gt; Java Compiler
+
+3.2 \u914D\u7F6EProject\u7684JDK\u7248\u672C
+Project Structure --&gt; Project Settings --&gt; project 
+
+3.3 \u914D\u7F6EModule\u4F9D\u8D56\u7684jdk\u7248\u672C\uFF08\u901A\u5E38\u4E0D\u7528\u7BA1\uFF09
+Project Structure --&gt; Project Settings --&gt; module
+
+3.4 \u68C0\u67E5\u5E73\u53F0\u7684\u7684JDK\u7248\u672C
+Project Structure --&gt; Platform Settings --&gt; SDKs
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_4-\u6784\u5EFA\u5DE5\u5177\u914D\u7F6E" tabindex="-1"><a class="header-anchor" href="#_4-\u6784\u5EFA\u5DE5\u5177\u914D\u7F6E" aria-hidden="true">#</a> 4 \u6784\u5EFA\u5DE5\u5177\u914D\u7F6E</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>\u4F4D\u7F6E\uFF1Abuild, Execution Depolyment --&gt; build tools--&gt; maven
+\u4FEE\u6539\u4EE5\u4E0B\u5185\u5BB9
+maven home path --&gt; \u4FEE\u6539\u6210\u81EA\u5DF1\u7684 maven
+User settings file --&gt; \u4FEE\u6539
+Local repository --&gt; \u4FEE\u6539
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_5-\u754C\u9762\u5C55\u793A" tabindex="-1"><a class="header-anchor" href="#_5-\u754C\u9762\u5C55\u793A" aria-hidden="true">#</a> 5 \u754C\u9762\u5C55\u793A</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>view --&gt; appearauce -&gt; toolbar \u6253\u94A9
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="_6-\u663E\u793A\u65B9\u6CD5\u7684\u5206\u9694\u7B26" tabindex="-1"><a class="header-anchor" href="#_6-\u663E\u793A\u65B9\u6CD5\u7684\u5206\u9694\u7B26" aria-hidden="true">#</a> 6 \u663E\u793A\u65B9\u6CD5\u7684\u5206\u9694\u7B26</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>\u4F4D\u7F6E\uFF1AFile | Settings | Editor | General | Appearance 
+\u4FEE\u6539\u4EE5\u4E0B
+show method separators \u6253\u94A9
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_7-\u63D0\u793A\u7684\u65F6\u5019-\u5FFD\u7565\u5927\u5C0F\u5199" tabindex="-1"><a class="header-anchor" href="#_7-\u63D0\u793A\u7684\u65F6\u5019-\u5FFD\u7565\u5927\u5C0F\u5199" aria-hidden="true">#</a> 7 \u63D0\u793A\u7684\u65F6\u5019\uFF0C\u5FFD\u7565\u5927\u5C0F\u5199</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>\u4F4D\u7F6E\uFF1AFile | Settings | Editor | General | Code Completion 
+\u4FEE\u6539\u5982\u4E0B
+Match case \u7684\u52FE\u53BB\u6389
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_8-\u4EE3\u7801\u7A97\u53E3\u6807\u9898\u4E0D\u9690\u85CF\u663E\u793A" tabindex="-1"><a class="header-anchor" href="#_8-\u4EE3\u7801\u7A97\u53E3\u6807\u9898\u4E0D\u9690\u85CF\u663E\u793A" aria-hidden="true">#</a> 8 \u4EE3\u7801\u7A97\u53E3\u6807\u9898\u4E0D\u9690\u85CF\u663E\u793A</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>\u4F4D\u7F6E\uFF1AFile | Settings | Editor | General | Editor Tabs 
+\u4FEE\u6539\u5982\u4E0B\u4E0B
+show tabs in one row \u6253\u94A9
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_9-\u5355\u884C\u4EE3\u7801\u4E0D\u6324\u5728\u4E00\u884C" tabindex="-1"><a class="header-anchor" href="#_9-\u5355\u884C\u4EE3\u7801\u4E0D\u6324\u5728\u4E00\u884C" aria-hidden="true">#</a> 9 \u5355\u884C\u4EE3\u7801\u4E0D\u6324\u5728\u4E00\u884C</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code> \u4F4D\u7F6E\uFF1AFile | Settings | Editor | General | Code Folding
+ \u4FEE\u6539\u5982\u4E0B
+ Java --&gt; One-line methods \u52FE\u53BB\u6389\u3002
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,18),t=[s];function r(l,c){return i(),n("div",null,t)}var o=e(d,[["render",r],["__file","settings.html.vue"]]);export{o as default};
